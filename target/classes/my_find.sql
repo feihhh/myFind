8ef9bbd42d0ba14b30100b8c91f44379433e29bc
feihhh <feihhh@xia.com>
2019-07-25 09:59:42 +0800

    项目依赖

--- target/classes/my_find.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists files(
-  name varchar (1024),
-  name_pinyin varchar(1024),
-  name_length int,
-  depth int ,
-  file_type varchar (10),
-  path varchar (2048)
-);
-create index if not exists index_name on files(name);
